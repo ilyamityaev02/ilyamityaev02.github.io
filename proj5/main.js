d160c24611ee16fcd48f40f3a9d5adeeddda6348
ilyamityaev02 <90443612+ilyamityaev02@users.noreply.github.com>
2021-09-13 20:44:49 +0300

    Update main.js

--- proj5/main.js
+++ proj5/main.js
@@ -1,17 +1,16 @@
-window.addEventListener("DOMContentLoaded", (e) => {
-  const b = document.getElementById("btn-count");
-  b.addEventListener("click", countPrice);
+window.addEventListener("DOMContentLoaded", function () {
+    const b = document.getElementById("btn-count");
+    b.addEventListener("click", countPrice);
 });
-
-function countPrice(e) {
-  e.preventDefault();
-  const price = document.getElementById("price");
-  const count = document.getElementById("num");
-  const total = document.getElementById("total");
-  let result = price.value * count.value;
-  if (!isNaN(Number(price.value)) && !isNaN(Number(count.value))) {
-    total.value = result;
-  } else {
-    alert("Допущена ошибка при вводе данных!");
-  }
+function countPrice(event) {
+    event.preventDefault();
+    const price = document.getElementById("price");
+    const count = document.getElementById("num");
+    const total = document.getElementById("total");
+    let result = price.value * count.value;
+    if (!Number.isNaN(result)) {
+        total.value = result;
+    } else {
+        alert("Допущена ошибка при вводе данных!");
+    }
 }
