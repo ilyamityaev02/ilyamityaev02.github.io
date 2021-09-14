949c4527592357ae7a05550de540143677985e13
ilyamityaev02 <90443612+ilyamityaev02@users.noreply.github.com>
2021-09-14 12:21:35 +0300

    Update main.js

--- proj7/main.js
+++ proj7/main.js
@@ -3,5 +3,21 @@ $(document).ready(function () {
     slidesToShow: 4,
     slidesToScroll: 1,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
