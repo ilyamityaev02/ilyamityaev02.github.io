d62b740360eb21b6c414894949eb837537b35d52
ilyamityaev02 <90443612+ilyamityaev02@users.noreply.github.com>
2021-09-15 18:12:47 +0300

    Add files via upload

--- proj8/script.js
+++ proj8/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  HidePopUp();
+  hidePopUp();
   window.history.pushState({ popup: "close" }, "popup", "./");
   window.addEventListener("popstate", function (e) {
-    if (e.state["popup"] == "close") {
-      HidePopUp();
+    if (e.state.popup === "close") {
+      hidePopUp();
     }
-    if (e.state["popup"] == "open") {
-      ShowPopUp();
+    if (e.state.popup === "open") {
+      showPopUp();
     }
   });
   const closeBtn = document.getElementById("close-popup");
-  closeBtn.addEventListener("click", HidePopUp);
+  closeBtn.addEventListener("click", hidePopUp);
   const popbtn = document.getElementById("popup-btn");
-  popbtn.addEventListener("click", ShowPopUp);
+  popbtn.addEventListener("click", showPopUp);
   const name = document.getElementById("name");
   const email = document.getElementById("email");
   const msg = document.getElementById("msg");
@@ -36,9 +36,9 @@ document.addEventListener("DOMContentLoaded", function () {
     xhr.responseType = "json";
     xhr.send(formData);
     xhr.onload = function () {
-      if (xhr.status != 200) {
+      if (xhr.status !== 200) {
         alert(
-          `Ошибка при выполнении запроса: ${xhr.status} - ${xhr.statusText} - ${xhr.response.message}`
+          `Ошибка при выполнении запроса: ${xhr.status} - ${xhr.response.message}`
         );
       } else {
         alert(`Запрос удачно обработан!`);
@@ -59,12 +59,12 @@ function toLocalStorage() {
   localStorage.setItem("email", email.value);
   localStorage.setItem("msg", msg.value);
 }
-function HidePopUp() {
+function hidePopUp() {
   window.history.replaceState({ popup: "close" }, "popup", "./");
   const popup = document.getElementById("popup");
   popup.style.display = "none";
 }
-function ShowPopUp() {
+function showPopUp() {
   window.history.replaceState({ popup: "open" }, "popup", "./form");
   const popup = document.getElementById("popup");
   popup.style.display = "block";
